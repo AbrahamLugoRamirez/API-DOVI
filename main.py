147dce413515346d35ce9d3cf5d675c229cf7797
kebinr <kebinr@uninorte.edu.co>
2020-11-14 09:05:51 -0500

    cors

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from distribution import *
 from distribution import Neigh
 from distribution import Colombia, States, Town, predict
 from distribution.Colombia import datasets
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(                                
+    CORSMiddleware,                                       
+    allow_origins=["*"],
+    allow_credentials=True,                             
+    allow_methods=["*"],                               
+    allow_headers=["*"]          
+)
 app.include_router(Colombia.router)
 app.include_router(States.router)
 app.include_router(Neigh.router)
